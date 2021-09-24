057c0d759312a68feda31f226ebab81e752e22f8
Dayeane <dayeane85@gmail.com>
2021-09-23 22:25:40 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solutions in this file
+// for (let age = 30; age < 40; age++) {
+//     console.log(`I'm ${age} years old. Happy birthday to me!`);
+    
+// }
+ 
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//   for (let i = 0; i < gifts.length; i++) {
+    // `Wrapped ${gifts[i]} and added a bow!`;
+  
+//   }
+
+//   return gifts;
+// }
+
+// wrapGifts(gifts);
+
+function writeCards(string_names, event_name) {
+    const message = []
+
+    for (let i = 0; i < string_names.length; i++) {
+        message.push(`Thank you, ${string_names[i]}, for the wonderful ${event_name} gift!`)
+    }
+    return message
+}
+
+function countDown(number) {
+    while (number > -1) {
+        console.log(number--)
+    }
+}
